2e03c457cff5fdca6085317bc32c522f9be05d92
CoderQiQin521 <coderqiqin@aliyun.com>
2018-12-09 19:28:26 +0800

    update template

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,4 @@
-console.log(345);
-var a = 100
+$(function() {
+  // code here...
 
-function test(params) {
-  console.log(params + a);
-  
-}
-
-// for(var i = 0; i < 99; i++) {
-//  console.log(i);
-// }
-if (true) {
-  console.log('大多数发多1少');
-  
-}
-
-test('水电费水电费')
+})
